524cb68cce8fadf529b7636f8f9e32cc7b3ee188
tvanom <tvanom1234@googlemail.com>
2019-05-21 19:20:24 +0100

    Added list crud functionality
    Added relevant functions for the list crud

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
 import React, {Component} from 'react';
 import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
 import Header from './components/layout/Header';
+import uuid from 'uuid';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   state = {
-    todos: []
+    todos: [
+        {id: uuid.v4(), title: "Eat breakfast", completed: false},
+        {id: uuid.v4(), title: "Eat lunch", completed: false},
+        {id: uuid.v4(), title: "Eat dinner", completed: false},
+        {id: uuid.v4(), title: "Sleep", completed: false}
+      ]
+  }
+
+  addItem = (title) => {
+    this.setState({
+      todos: [...this.state.todos,{
+        id: uuid.v4(),
+        title: title,
+        completed: false
+      }]
+    });
+  }
+
+  deleteItem = (id) => {
+    this.setState({todos: [...this.state.todos.filter(todo => 
+      todo.id !== id
+    )]});
+  }
+
+  editItem = (id,title) => {
+    this.setState({todo: [...this.state.todos.map(todo => {
+        if(id === todo.id)
+          todo.title = title;
+      })
+    ]});
+  }
+
+  toggleItem = (id) => {
+    this.setState({todos: this.state.todos.map(todo => {
+        if(todo.id === id)
+          todo.completed = !todo.completed;
+
+        return todo;
+      })
+    });
   }
 
   render(){
     return (
       <div className="App">
         <Header />
-        <TodoList todoItems={this.state.todos}/>
+        <AddTodo addTodo={this.addItem} />
+        <TodoList 
+          todoItems  = {this.state.todos} 
+          toggleItem = {this.toggleItem}
+          editItem   = {this.editItem}
+          deleteItem = {this.deleteItem}
+          />
       </div>
     );
   }
